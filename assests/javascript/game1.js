8edbe0d12679ef25f02b9718fbd02a72aaa8d0e2
tanajeannai3 <tanajeannai3@gmail.com>
2019-10-11 00:38:41 -0500

    update

--- assests/javascript/game1.js
+++ assests/javascript/game1.js
@@ -1,14 +1,17 @@
 var word = [
     "serenade", 
-    "highest in the room", 
+    "HITR", 
     "impossible", 
-    "butterfly effect",];
+    "antidote",];
 var randomWord = word[Math.floor(Math.random() * word.length)];
 var answerArray = [];
 var wrongGuess = [];
-var leftGuesses = 10;
 var wins = 0;
 var losses = 0;
+
+var leftGuesses = 10;
+var ele = document.getElementById('remaining');
+ele.innerHTML=leftGuesses;
 
 for(var i = 0; i < word.length; i++){
     answerArray[i] = '_';
@@ -19,15 +22,36 @@ document.onkeyup = function(event) {
     console.log(randomWord);
     
     for(var j = 0; j < randomWord.length; j++){
-        
+        if(randomWord[j]===userGuess){
+            answerArray[j]=userGuess;
+        }
     }//END 2ND FOR LOOP 
+
+    if(userGuess !== randomWord[j]){
+        wrongGuess.push(userGuess);
+        leftGuesses--;
+        var wg = document.getElementById('wrong');
+        wg.innerHTML=wrongGuesses;
+    }
+    if(leftGuesses == 0){
+        console.log("You loser...")
+    }
+    if(answerArray == randomWord){
+        console.log("You are so right!");
+    }
+
+    var el = document.getElementById('guessword');
+    el.innerHTML=guessComposer.join(" ");
+
+    var ele = document.getElementById('remaining');
+    ele.innerHTML=guessesLeft;
 
     var winsText = document.getElementById("wins-text");
     var lossesText = document.getElementById("losses-text");
     var userChoiceText = document.getElementById("userchoice-text");
-    var remainingLettersText = document.getElementById("remainingGuesses-text");
+    // var remainingLettersText = document.getElementById("remainingGuesses-text");
     userChoiceText.textContent = "Letters Already Guessed: " + userGuess;
     winsText.textContent = "Wins: " + wins;
     lossesText.textContent = "Losses: " + losses;
-    remainingLettersText.textContent = "Number of Guesses Remaining:" + remainingGuesses;
+    // remainingLettersText.textContent = "Number of Guesses Remaining:" + remainingGuesses;
 };//END DOC 
